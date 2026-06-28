38184dc8cd1d49778b6b9bdd53be1a2cf33718b5
4MHARK <mharkdavid713@gmail.com>
2026-06-28 22:19:06 +0100

    fix(server): enable CORS for frontend auth requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,12 @@
 // Create the application
-
+import cors from "cors";
 import express, { json } from "express"
 import indexRoutes from "./routes/index.js"
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
+
 app.use(express.json())
 app.get("/", (req, res) =>{
     res.json({
