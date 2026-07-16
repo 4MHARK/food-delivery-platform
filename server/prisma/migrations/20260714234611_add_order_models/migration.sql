12589a24a2a37048e2078be0d19a8d89ca0a4606
4MHARK <mharkdavid713@gmail.com>
2026-07-16 20:50:22 +0100

    Add order routes with POST and GET endpoints

---
+++ server/prisma/migrations/20260714234611_add_order_models/migration.sql
@@ -0,0 +1,41 @@
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'CONFIRMED', 'PREPARING', 'READY', 'DELIVERING', 'CANCELLED');
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "status" "OrderStatus" NOT NULL DEFAULT 'PENDING',
+    "total" DOUBLE PRECISION NOT NULL,
+    "deliveryAddress" TEXT NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "restaurantId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" SERIAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "menuItemId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_menuItemId_fkey" FOREIGN KEY ("menuItemId") REFERENCES "MenuItem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ server/src/routes/order.routes.js
@@ -0,0 +1,148 @@
+import express from "express";
+import prisma from "../config/prisma.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+// import ownerMiddleware from "../middleware/owner.middleware.js";
+
+const router = express.Router();
+
+router.post("/orders", authMiddleware, async (req, res) => {
+  try {
+    const { restaurantId, deliveryAddress, items } = req.body;
+
+    if (!items || items.length === 0) {
+      return res.status(400).json({
+        message: "Order must have at least one item",
+      });
+    }
+    if (!restaurantId) {
+      return res.status(400).json({
+        message: "Restaurant id is required",
+      });
+    }
+    if (!deliveryAddress) {
+      return res.status(400).json({
+        message: "Delivery address is required",
+      });
+    }
+
+    let total = 0;
+    for (const item of items) {
+      const menuItem = await prisma.menuItem.findUnique({
+        where: { id: item.menuItemId },
+      });
+
+      if (!menuItem) {
+        return res.status(404).json({
+          message: `Menu Item ${item.menuItemId} not found`,
+        });
+      }
+      total += menuItem.price * item.quantity;
+      item.price = menuItem.price;
+    }
+
+    const order = await prisma.order.create({
+      data: {
+        customerId: req.user.id,
+        restaurantId: Number(restaurantId),
+        deliveryAddress,
+        total,
+        orderItems: {
+          create: items.map((item) => ({
+            quantity: item.quantity,
+            price: item.price,
+            menuItemId: item.menuItemId,
+          })),
+        },
+      },
+      include: {
+        orderItems: true,
+        restaurant: true,
+      },
+    });
+
+    res.status(201).json({
+      message: "Order Placed scuccessfully",
+      order,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: " Server is down",
+      error: error.message,
+    });
+  }
+});
+
+router.get("/orders", authMiddleware, async (req, res) => {
+  try {
+    const orders = await prisma.order.findMany({
+      where: { customerId: req.user.id },
+      include: {
+        orderItems: true,
+        restaurant: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.status(200).json({
+      message: "Order fetched successfully",
+      orders,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+});
+
+router.get("/orders/:id", authMiddleware, async (req, res) => {
+  try {
+    const order = await prisma.order.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+      include: {
+        orderItems: {
+          include: {
+            menuItem: true,
+          },
+        },
+        restaurant: {
+          include: {
+            owner: {
+              select:{ id: true, name: true, email: true}
+            }
+          },
+        },
+      },
+    });
+    if (!order) {
+      return res.status(404).json({
+        message: "Order not found",
+      });
+    }
+
+const isCustomer = order.customerId === req.user.id;
+    const isOwner = order.restaurant.ownerId === req.user.id;
+
+    if (!isCustomer && !isOwner) {
+      return res.status(403).json({
+        message: "You are not allowed to view this order",
+      });
+    }
+
+    res.status(200).json({
+      message: "Order fetched successfully",
+      order,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+});
+
+export default router;
